4e071520daf0250911ddfc6f42f2faed4ce7f6c3
ha i <hai@MacBookPro.lan>
2026-08-04 16:56:42 -0400

    ssh enable

--- infra/aws-starter/main.tf
+++ infra/aws-starter/main.tf
@@ -39,7 +39,7 @@ resource "aws_security_group" "instance" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.ssh_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "api-gateway"
